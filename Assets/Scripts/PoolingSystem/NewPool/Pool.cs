2dfe200671d7076d87c0978109ef57e46361da1c
Orstein401 <alerom342@gmail.com>
2026-03-18 19:58:01 +0100

    Ver1.0

--- Assets/Scripts/PoolingSystem/NewPool/Pool.cs
+++ Assets/Scripts/PoolingSystem/NewPool/Pool.cs
@@ -9,19 +9,12 @@ public class Pool : Singleton<Pool>
 
     public void CreatePool(PoolSettings poolSetting)
     {
-        //var list = new List<GameObject>();
-        //for (int i = 0; i < poolSetting.DefaultSize; i++)
-        //{
-        //    var obj = Instantiate(poolSetting.Prefab, transform);
-        //    obj.SetActive(false);
-        //    list.Add(obj);
-        //}    
-        //pools.Add(poolSetting.Id, list);
         var list = new List<GameObject>();
         for (int i = 0; i < poolSetting.numPerPrefab; i++)
         {
             foreach(var prefab in poolSetting.Prefabs)
             {
+                Debug.Log(prefab);
                 var obj = Instantiate(prefab, transform);
                 obj.SetActive(false);
                 list.Add(obj);
@@ -30,9 +23,9 @@ public class Pool : Singleton<Pool>
         pools.Add(poolSetting.Id, list);
     }
 
-    public GameObject GetItemPool(PoolId id)
+    public GameObject GetItemPool(PoolSettings poolType)
     {
-        if (pools.TryGetValue(id, out List<GameObject> list))
+        if (pools.TryGetValue(poolType.Id, out List<GameObject> list))
         {
             //Debug.Log(list.Count + "lista");
             int index = Random.Range(0, list.Count);
@@ -42,6 +35,7 @@ public class Pool : Singleton<Pool>
             list.RemoveAt(index);
             return obj;
         }
+        Debug.LogWarning($"Non ci sono ItemPool nella pool di questo tipo: {poolType.Id} ne creo degli altri");
         return null;
     }
     public void ReturnToPool(PoolId id, GameObject obj)
@@ -55,3 +49,15 @@ public class Pool : Singleton<Pool>
 
     }
 }
+
+//public void CreatePool(PoolSettings poolSetting)
+//{
+//    //var list = new List<GameObject>();
+//    //for (int i = 0; i < poolSetting.DefaultSize; i++)
+//    //{
+//    //    var obj = Instantiate(poolSetting.Prefab, transform);
+//    //    obj.SetActive(false);
+//    //    list.Add(obj);
+//    //}    
+//    //pools.Add(poolSetting.Id, list);
+//}

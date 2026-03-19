f0c3b09a036734db411eee135548db31673308d4
Lavinia <laviniaungaro@gmail.com>
2026-03-19 21:41:43 +0100

    Added SoundID to buttons and GameOver

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -7,6 +7,9 @@ public class GameManager : MonoBehaviour
     [SerializeField] private UnityEvent _onGameOver;
     [SerializeField] private UnityEvent _onWin;
 
+    [SerializeField] SoundID buttonSoundID;
+    [SerializeField] SoundID musicID;
+
     public void GameOver()
     {
         Invoke(nameof(DelayGameOver), 1);
@@ -15,29 +18,30 @@ public class GameManager : MonoBehaviour
     public void DelayGameOver()
     {
         _onGameOver?.Invoke();
-        // inserire suono di GameOver
-        // se serve StopBackgroundMusic()
+        AudioManager.Instance.PlayMusic(musicID);
     }
 
-    public void Win()
-    {
-        Invoke(nameof(DelayWin), 1);
-    }
-
-    public void DelayWin()
-    {
-        _onWin?.Invoke();
-        // inserire suono di Win
-        // se serve StopBackgroundMusic()
-    }
+    // gestione bottoni
 
     public void MainMenu()
     {
+        Invoke(nameof(DelayMainMenu), 1);
+    }
+
+    public void DelayMainMenu()
+    {
+        AudioManager.Instance.PlaySound(buttonSoundID);
         SceneManager.LoadScene("MainMenu");
     }
 
     public void Retry()
     {
+        Invoke(nameof(DelayRetry), 1);
+    }
+
+    public void DelayRetry()
+    {
+        AudioManager.Instance.PlaySound(buttonSoundID);
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }

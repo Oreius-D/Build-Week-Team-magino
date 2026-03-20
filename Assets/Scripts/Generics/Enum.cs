a4c7d6121be8b95535152d11a926ffed9984f97a
Orstein401 <alerom342@gmail.com>
2026-03-20 19:27:02 +0100

    ver1.7

--- Assets/Scripts/Generics/Enum.cs
+++ Assets/Scripts/Generics/Enum.cs
@@ -3,4 +3,4 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public enum SoundID { Music, Coin }
-public enum PoolId {MapForest, MapDesert,MapIce, Coin}
+public enum PoolId {MapForest, MapDesert,MapCity, Coin}

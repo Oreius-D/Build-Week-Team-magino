777805c2add99dea1f6b6423de5f38031c6a4542
Orstein401 <alerom342@gmail.com>
2026-05-05 11:29:13 +0200

    Ver1.0
    AudioManager implementato e corretto

--- Assets/Scripts/Generics/Enum.cs
+++ Assets/Scripts/Generics/Enum.cs
@@ -2,5 +2,5 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum SoundID { Music, Coin }
+public enum SoundID { Music, Coin, Buy, Button }
 public enum PoolId {MapForest, MapDesert,MapCity, Coin}
